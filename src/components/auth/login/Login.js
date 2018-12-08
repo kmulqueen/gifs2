33cda3bfd4e8d504f516eb152fc02eb953274d78
Kyle Mulqueen <kmulqdrums710@gmail.com>
2018-12-08 16:42:12 -0700

    log in works

--- src/components/auth/login/Login.js
+++ src/components/auth/login/Login.js
@@ -1,35 +1,25 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { logIn } from "../../../store/actions/authActions";
 import "./Login.css";
 
 class Login extends Component {
   state = {
     email: "",
-    password: "",
-    error: ""
+    password: ""
   };
-
-  dismissError = () => this.setState({ error: "" });
 
   onSubmit = evt => {
     evt.preventDefault();
 
-    if (!this.state.email) {
-      return this.setState({ error: "Email is required" });
-    }
-
-    if (!this.state.password) {
-      return this.setState({ error: "Password is required" });
-    }
-
     // Log In with Firebase
-
-    // Clear any errors
-    return this.setState({ error: "" });
+    this.props.logIn(this.state);
   };
 
   onChange = evt => this.setState({ [evt.target.name]: evt.target.value });
 
   render() {
+    const { authError } = this.props;
     return (
       <div className="container">
         <div className="row login-row col-12">
@@ -39,12 +29,6 @@ class Login extends Component {
 
           <div className="Login col-12">
             <form onSubmit={this.onSubmit}>
-              {this.state.error && (
-                <h3 data-test="error" onClick={this.dismissError}>
-                  <button onClick={this.dismissError}>✖</button>
-                  {this.state.error}
-                </h3>
-              )}
               <div className="col-12">
                 <label>Email</label>
                 <input
@@ -66,6 +50,12 @@ class Login extends Component {
                 />
               </div>
               <input type="submit" value="Log In" data-test="submit" />
+              {/* Check for Login Errors */}
+              {authError ? (
+                <div className="alert alert-danger" role="alert">
+                  {authError}
+                </div>
+              ) : null}
             </form>
           </div>
         </div>
@@ -73,4 +63,20 @@ class Login extends Component {
     );
   }
 }
-export default Login;
+
+const mapStateToProps = state => {
+  return {
+    authError: state.auth.authError
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    logIn: creds => dispatch(logIn(creds))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Login);
