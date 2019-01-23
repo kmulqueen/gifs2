67561cca5e0d1205fa57151eee904e6a117eee35
Josephine Aguilar <josieamor@Josephines-MacBook-Pro.local>
2019-01-22 18:14:59 -0700

    This is what I updated with the timer stuff

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import "./Footer.css";
 import Search from "../Search";
+import Timer from "../Timer";
 import GifCard from "../GifCard";
 
 class Footer extends Component {
   render() {
+    
     return (
       <div className="footer">
         <div className="col-3 left-side">
-          <div className="timer">This is where the timer goes</div>
+        <div >Timer: </div>
           <div className="points">
             <div className="losses" />
             <div className="wins" />
